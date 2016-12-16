7ac190ee21a80ad46390c41721bef779e578f3f0
Jon Lighthall <jlighthall@fsu.edu>
2016-12-16 23:33:29 +0000

    Summary: added user query for deletion of evt files; updated to evt_files for mADC compatibility

--- data.cpp
+++ data.cpp
@@ -12,17 +12,24 @@ int main()
   
   ifstream infile(fname);
   printf("Reading list file \"%s\"\n",fname);
-  printf(" The following runs will be read\n");
+  printf(" The following runs will be converted\n");
   int i=0;
   while (infile >> list[i]) {
     printf("  %d\n",list[i]);
     i++;
   }
-  
+
+  char ans;
+  bool del=false;
+  cout << " Do you want to delete the .evt files after conversion?" << endl << "  ";
+  cin >> ans;
+  if (ans=='y')
+    del=true;
+      
   for(int j=0;j<i;j++)
     {
-      std::ofstream outfile("data_files.list"); //name of file referenced in evt2root_NSCL11.C
-      string str0 = "/data1/lighthall/data2/"; //location of .evt files
+      std::ofstream outfile("evt_files.list"); //name of file referenced in evt2root_NSCL11.C
+      string str0 = "/data1/lighthall/data/"; //location of .evt files
       outfile << "Output ROOT file: /data1/lighthall/root/run" << list[j] << ".root" << endl;
       outfile << "Data directory: "<< str0 << endl;
       outfile << list[j] << endl;
@@ -37,6 +44,7 @@ int main()
       num << list[j];
       str=num.str();
       string str3 =  str1+str+str2;
-      system(str3.c_str()); //use this line to delete the .evt files after conversion
+      if(del)
+	system(str3.c_str()); //use this line to delete the .evt files after conversion
     }
 }
