0e54ca3a7477108f92d6b85eb609f20045975e2a
Jon Lighthall <jlighthall@fsu.edu>
2017-06-19 12:06:39 -0400

    formatting changes

--- 2015_detclass.h
+++ 2015_detclass.h
@@ -9,62 +9,59 @@ using namespace std;
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
 class ASICHit {
-// This class is for ASICs hit
-public:
-	ASICHit(){};
-	Int_t Nhits,MBID[MaxHits],CBID[MaxHits],ChNum[MaxHits];
-        Int_t Energy[MaxHits];
-        Int_t Time[MaxHits];
+  // This class is for ASICs hit
+ public:
+  ASICHit(){};
+  Int_t Nhits,MBID[MaxHits],CBID[MaxHits],ChNum[MaxHits];
+  Int_t Energy[MaxHits];
+  Int_t Time[MaxHits];
 
-        void ResetASICHit()
-	{
-	   Nhits = 0;
+  void ResetASICHit() {
+    Nhits = 0;
 
-           for (Int_t i=0; i<MaxHits;i++) {
-              MBID[i] = 0;
-              CBID[i] = 0;
-              ChNum[i] = 0;
-              Energy[i] = 0;
-              Time[i]  = 0;
-           } 	
-        };
+    for (Int_t i=0; i<MaxHits;i++) {
+      MBID[i] = 0;
+      CBID[i] = 0;
+      ChNum[i] = 0;
+      Energy[i] = 0;
+      Time[i]  = 0;
+    } 	
+  };
 };
 ////////////////////////////////////////////////////////
 class CAENHit {
-//This class is for CAEN hit
-public:
-	CAENHit(){};
-	Int_t Nhits,ID[MaxCaenHits],ChNum[MaxCaenHits];
-        Int_t Data[MaxCaenHits];
+  //This class is for CAEN hit
+ public:
+  CAENHit(){};
+  Int_t Nhits,ID[MaxCaenHits],ChNum[MaxCaenHits];
+  Int_t Data[MaxCaenHits];
 
-        void ResetCAENHit()
-	{
-	    Nhits = 0;
+  void ResetCAENHit() {
+    Nhits = 0;
 
-            for (Int_t i=0; i<MaxCaenHits;i++) {
-              ID[i] = 0;
-              ChNum[i] = 0;
-              Data[i]  = 0;
-           }
-         };
+    for (Int_t i=0; i<MaxCaenHits;i++) {
+      ID[i] = 0;
+      ChNum[i] = 0;
+      Data[i]  = 0;
+    }
+  };
 };
 ////////////////////////////////////////////////////////
 class MesyHit {
-//This class is for MesyTech hit
-public:
-	MesyHit(){};
-	Int_t Nhits,ID[MaxMesyHits],ChNum[MaxMesyHits];
-        Int_t Data[MaxMesyHits];
+  //This class is for MesyTech hit
+ public:
+  MesyHit(){};
+  Int_t Nhits,ID[MaxMesyHits],ChNum[MaxMesyHits];
+  Int_t Data[MaxMesyHits];
 
-        void ResetMesyHit()
-	{
-	    Nhits = 0;
+  void ResetMesyHit() {
+    Nhits = 0;
 
-            for (Int_t i=0; i<MaxMesyHits;i++) {
-              ID[i] = 0;
-              ChNum[i] = 0;
-              Data[i]  = 0;
-           }
-         };
+    for (Int_t i=0; i<MaxMesyHits;i++) {
+      ID[i] = 0;
+      ChNum[i] = 0;
+      Data[i]  = 0;
+    }
+  };
 };
 ////////////////////////////////////////////////////////
